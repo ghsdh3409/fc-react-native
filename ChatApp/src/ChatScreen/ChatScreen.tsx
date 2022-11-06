b55f86d6d1b899c500b17eebd05e3ee3b75bce97
Daehoon Kim <ghsdh3409@gmail.com>
2022-11-06 13:56:52 +0900

    CH1-11. 메시지 불러오기 기능 구현하기

--- ChatApp/src/ChatScreen/ChatScreen.tsx
+++ ChatApp/src/ChatScreen/ChatScreen.tsx
@@ -93,10 +93,14 @@ const disabledSendButtonStyle = [
 const ChatScreen = () => {
   const { params } = useRoute<RouteProp<RootStackParamList, 'Chat'>>();
   const { other, userIds } = params;
-  const { loadingChat, chat, sendMessage } = useChat(userIds);
+  const { loadingChat, chat, sendMessage, messages, loadingMessages } =
+    useChat(userIds);
   const [text, setText] = useState('');
   const sendDisabled = useMemo(() => text.length === 0, [text]);
   const { user: me } = useContext(AuthContext);
+  const loading = loadingChat || loadingMessages;
+
+  console.log('messages', messages);
 
   const onChangeText = useCallback((newText: string) => {
     setText(newText);
@@ -127,7 +131,19 @@ const ChatScreen = () => {
             horizontal
           />
         </View>
-        <View style={styles.messageList} />
+        <FlatList
+          style={styles.messageList}
+          data={messages}
+          renderItem={({ item: message }) => {
+            return (
+              <View>
+                <Text>{message.user.name}</Text>
+                <Text>{message.text}</Text>
+                <Text>{message.createdAt.toISOString()}</Text>
+              </View>
+            );
+          }}
+        />
         <View style={styles.inputContainer}>
           <View style={styles.textInputContainer}>
             <TextInput
@@ -146,12 +162,12 @@ const ChatScreen = () => {
         </View>
       </View>
     );
-  }, [chat, onChangeText, text, sendDisabled, onPressSendButton]);
+  }, [chat, onChangeText, text, sendDisabled, onPressSendButton, messages]);
 
   return (
     <Screen title={other.name}>
       <View style={styles.container}>
-        {loadingChat ? (
+        {loading ? (
           <View style={styles.loadingContainer}>
             <ActivityIndicator />
           </View>
