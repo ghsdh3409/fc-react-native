7b6dea881da3671a47e3ac8cdf23dd2483b8c4f3
Daehoon Kim <ghsdh3409@gmail.com>
2022-11-20 16:28:28 +0900

    CH2-3. API 이용해서 데이터 불러오기

--- MovieReminder/App.tsx
+++ MovieReminder/App.tsx
@@ -1,18 +1,22 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import MoviesScreen from './src/screens/MoviesScreen/MoviesScreen';
 import { RootStackParamList } from './src/types';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
+const queryClient = new QueryClient();
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Movies" component={MoviesScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <QueryClientProvider client={queryClient}>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Movies" component={MoviesScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </QueryClientProvider>
   );
 };
 
