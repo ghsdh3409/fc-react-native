7c19e832e8a55ba65822a8c9f891b7b31a8a749f
Daehoon Kim <ghsdh3409@gmail.com>
2022-12-16 13:34:09 +0900

    CH5-4. CodePush 셋업 - iOS

--- MovieReminder/ios/MovieReminder/AppDelegate.mm
+++ MovieReminder/ios/MovieReminder/AppDelegate.mm
@@ -16,6 +16,8 @@
 
 #import <react/config/ReactNativeConfig.h>
 
+#import <CodePush/CodePush.h>
+
 static NSString *const kRNConcurrentRoot = @"concurrentRoot";
 
 @interface AppDelegate () <RCTCxxBridgeDelegate, RCTTurboModuleManagerDelegate> {
@@ -87,7 +89,7 @@ static NSString *const kRNConcurrentRoot = @"concurrentRoot";
 #if DEBUG
   return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
 #else
-  return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+  return [CodePush bundleURL];
 #endif
 }
 
